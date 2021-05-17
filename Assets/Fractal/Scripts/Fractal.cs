ef510a5f2c3910e7842c4df5ac42844abf3b76fb
Reckoning47 <day0fdiligence@gmail.com>
2021-05-17 16:46:08 -0600

    shaders not working again

--- Assets/Fractal/Scripts/Fractal.cs
+++ Assets/Fractal/Scripts/Fractal.cs
@@ -16,8 +16,10 @@ public class Fractal : MonoBehaviour
 
 	FractalPart[][] parts;
 	Matrix4x4[][] matrices;
+	ComputeBuffer[] matricesBuffers;
 
-
+	static readonly int matricesId = Shader.PropertyToID("_Matrices");
+	static MaterialPropertyBlock propertyBlock;
 
 	struct FractalPart
 	{
@@ -40,16 +42,17 @@ public class Fractal : MonoBehaviour
 
 
 
-	void Awake()
+	void OnEnable()
 	{
-		
-
 		parts = new FractalPart[depth][];
 		matrices = new Matrix4x4[depth][];
+		matricesBuffers = new ComputeBuffer[depth];
+		int stride = 16 * 4;
 		for (int i = 0, length = 1; i < parts.Length; i++, length *= 5)
 		{
 			parts[i] = new FractalPart[length];
-			matrices[i] = new Matrix4x4[length];
+			matrices[i] = new Matrix4x4[length]; 
+			matricesBuffers[i] = new ComputeBuffer(length, stride);
 		}
 
 		//CreatePart(0);
@@ -64,13 +67,37 @@ public class Fractal : MonoBehaviour
 				}
 			}
 		}
+		if (propertyBlock == null)
+		{
+			propertyBlock = new MaterialPropertyBlock();
+		}
+	}
+
+	void OnDisable()
+	{
+		for (int i = 0; i < matricesBuffers.Length; i++)
+		{
+			matricesBuffers[i].Release();
+		}
+		parts = null;
+		matrices = null;
+		matricesBuffers = null;
+	}
+	void OnValidate()
+	{
+		if (parts != null && enabled)
+		{
+			OnDisable();
+			OnEnable();
+		}
 	}
 	void Update()
 	{
-		Quaternion deltaRotation = Quaternion.Euler(0f, 22.5f * Time.deltaTime, 0f);
+		float spinAngleDelta = 22.5f * Time.deltaTime;
 		FractalPart rootPart = parts[0][0];
-		rootPart.rotation *= deltaRotation;
-		rootPart.worldRotation = rootPart.rotation;
+		rootPart.spinAngle += spinAngleDelta;
+		rootPart.worldRotation =
+			rootPart.rotation * Quaternion.Euler(0f, rootPart.spinAngle, 0f);
 		parts[0][0] = rootPart;
 		matrices[0][0] = Matrix4x4.TRS(
 			rootPart.worldPosition, rootPart.worldRotation, Vector3.one
@@ -86,16 +113,34 @@ public class Fractal : MonoBehaviour
 			{
 				FractalPart parent = parentParts[fpi / 5];
 				FractalPart part = levelParts[fpi];
-				part.worldRotation = parent.worldRotation * part.rotation;
+				part.spinAngle += spinAngleDelta;
+				part.worldRotation =
+					parent.worldRotation *
+					(part.rotation * Quaternion.Euler(0f, part.spinAngle, 0f));
 				part.worldPosition =
 					parent.worldPosition +
 					parent.worldRotation * (1.5f * scale * part.direction);
+
+
 				levelParts[fpi] = part;
 
 				levelMatrices[fpi] = Matrix4x4.TRS(
 					part.worldPosition, part.worldRotation, scale * Vector3.one
 				);
 			}
+		}
+		for (int i = 0; i < matricesBuffers.Length; i++)
+		{
+			matricesBuffers[i].SetData(matrices[i]);
+		}
+		var bounds = new Bounds(Vector3.zero, 3f * Vector3.one);
+		for (int i = 0; i < matricesBuffers.Length; i++)
+		{
+			ComputeBuffer buffer = matricesBuffers[i];
+			buffer.SetData(matrices[i]);
+			propertyBlock.SetBuffer(matricesId, buffer);
+            material.SetBuffer(matricesId, buffer);
+            Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, buffer.count, propertyBlock);
 		}
 	}
 	FractalPart CreatePart(int childIndex)
