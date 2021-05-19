c8bdb868fe0a78ab0f75805a2090e04fd75e9f38
Reckoning47 <day0fdiligence@gmail.com>
2021-05-19 15:26:14 -0600

    upgraded versions and got fractal working

---
+++ Assets/Compute Shaders/Scripts/Targeter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+public class Targeter : MonoBehaviour
+{
+    Camera cam;
+    Vector3 pos;
+
+    [SerializeField] Transform target;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+    void Update()
+    {
+        pos = cam.WorldToScreenPoint(target.position);
+        Ray ray = cam.ScreenPointToRay(pos);
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+    }
+}
