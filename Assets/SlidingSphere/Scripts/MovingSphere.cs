0b1150778773af61d162c994d8131449d99d1df6
Reckoning47 <day0fdiligence@gmail.com>
2021-05-21 12:26:14 -0600

    switch to DRP

--- Assets/SlidingSphere/Scripts/MovingSphere.cs
+++ Assets/SlidingSphere/Scripts/MovingSphere.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class MovingSphere : MonoBehaviour
 {
-    //TrailRenderer trailRenderer;
-    // Start is called before the first frame update
+    [SerializeField, Range(0f, 100f)]
+    float maxSpeed = 10f;
+
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f;
+
+
+    [SerializeField]
+    Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
+
+    [SerializeField, Range(0f, 1f)]
+    float bounciness = 0.5f;
+
+    Vector3 velocity;
+
     void Start()
     {
-        
     }
 
     void Update()
@@ -19,8 +31,40 @@ public class MovingSphere : MonoBehaviour
 
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
 
-        Vector3 displacement = new Vector3(playerInput.x, 0f, playerInput.y);
-        transform.localPosition += displacement;
-        //transform.localPosition = new Vector3(playerInput.x, 0.5f, playerInput.y);
+        Vector3 acceleration =
+             new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        Vector3 desiredVelocity =
+            new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+
+        velocity.x =
+            Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z =
+            Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
+        Vector3 displacement = velocity * Time.deltaTime;
+        Vector3 newPosition = transform.localPosition + displacement;
+        if (newPosition.x < allowedArea.xMin)
+        {
+            newPosition.x = allowedArea.xMin;
+            velocity.x = -velocity.x * bounciness;
+        }
+        else if (newPosition.x > allowedArea.xMax)
+        {
+            newPosition.x = allowedArea.xMax;
+            velocity.x = -velocity.x * bounciness;
+        }
+        if (newPosition.z < allowedArea.yMin)
+        {
+            newPosition.z = allowedArea.yMin;
+            velocity.z = -velocity.z * bounciness;
+        }
+        else if (newPosition.z > allowedArea.yMax)
+        {
+            newPosition.z = allowedArea.yMax;
+            velocity.z = -velocity.z * bounciness;
+        }
+        transform.localPosition = newPosition;
     }
 }
