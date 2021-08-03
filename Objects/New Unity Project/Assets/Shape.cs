d80090b955791a4b8fea76b7d5e8998604c34f02
Reckoning47 <day0fdiligence@gmail.com>
2021-08-03 12:01:22 -0600

    spawn zones

---
+++ Objects/New Unity Project/Assets/Shape.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+
+public class Shape : PersistableObject
+{
+	int shapeId;
+	public int ShapeId
+	{
+		get
+		{
+			return shapeId;
+		}
+		set
+		{
+			if (shapeId == 0)
+			{
+				shapeId = value;
+			}
+			else
+			{
+				Debug.LogError("Not allowed to change shapeId.");
+			}
+		}
+	}
+
+	
+}
